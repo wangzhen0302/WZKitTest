227a299de2682a7fabb0cc136bd023b3fa82bec2
zhen.wang <zhen.wang@broadlink.com.cn>
2022-03-23 16:59:54 +0800

    初始化仓库

---
+++ Example/Pods/Target Support Files/Pods-WZKitTest_Example/Pods-WZKitTest_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WZKitTest_Example : NSObject
+@end
+@implementation PodsDummy_Pods_WZKitTest_Example
+@end
---
+++ Example/Pods/Target Support Files/Pods-WZKitTest_Tests/Pods-WZKitTest_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WZKitTest_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_WZKitTest_Tests
+@end
