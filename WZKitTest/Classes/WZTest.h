5c8ecf221177a1d83272055190286ebfed5f37c7
zhen.wang <zhen.wang@broadlink.com.cn>
2022-03-23 18:11:01 +0800

    第二个版本

---
+++ WZKitTest/Classes/WZTest.h
@@ -0,0 +1,16 @@
+//
+//  WZTest.h
+//  WZKitTest
+//
+//  Created by 王振 on 2022/3/23.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WZTest : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ WZKitTest/Classes/WZTest.m
@@ -0,0 +1,12 @@
+//
+//  WZTest.m
+//  WZKitTest
+//
+//  Created by 王振 on 2022/3/23.
+//
+
+#import "WZTest.h"
+
+@implementation WZTest
+
+@end
